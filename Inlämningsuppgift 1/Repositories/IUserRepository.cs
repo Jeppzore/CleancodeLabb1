c1654c7a4d7f36a409f8fa4dec36701a5f9e96d3
Jesper Stranne <179859375+Jeppzore@users.noreply.github.com>
2025-11-24 15:10:19 +0100

    Created Repository classes and interfaces for the available services (user, cart, order, product).

---
+++ Inlämningsuppgift 1/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using static Inlämningsuppgift_1.Services.UserService;
+
+namespace Inlämningsuppgift_1.Repositories
+{
+    public interface IUserRepository
+    {
+        User? GetById(int id);
+        User? GetByUsername(string username);
+        void Add(User user);
+    }
+}
