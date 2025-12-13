2efc1e645f2e239c63716531d49f4bfbc26f5a22
Jesper Stranne <179859375+Jeppzore@users.noreply.github.com>
2025-12-13 10:21:30 +0100

    Created Inlämningsuppgift 1.Tests
    Created a project for testing purposes with xunit and moq
    
    Created a simple structure with Services and Controllers folders and empty classes.

---
+++ Inlämningsuppgift 1.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift_1.Tests.Services
+{
+    internal class CartServiceTests
+    {
+    }
+}
