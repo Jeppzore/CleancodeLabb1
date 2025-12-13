9c2b28f9e5ea7a6adf0b3b241291ef930ebef5f4
Jesper Stranne <179859375+Jeppzore@users.noreply.github.com>
2025-12-13 21:29:22 +0100

    Update OrderControllerTests.cs
    Added a single test to test if the OrderController doesn't create an order with a unauthorized usertoken

--- Inlämningsuppgift 1.Tests/Controllers/OrderControllerTests.cs
+++ Inlämningsuppgift 1.Tests/Controllers/OrderControllerTests.cs
@@ -1,7 +1,29 @@
-﻿namespace Inlämningsuppgift_1.Tests.Controllers
+﻿using Inlämningsuppgift_1.Controllers;
+using Inlämningsuppgift_1.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Inlämningsuppgift_1.Tests.Controllers
 {
     public class OrderControllerTests
     {
+        [Fact]
+        public async Task CreateFromCart_ReturnsUnauthorized_WhenTokenInvalid()
+        {
+            // Arrange
+            var orderService = new Mock<IOrderService>();
+            var authService = new Mock<IAuthService>();
 
+            authService.Setup(a => a.GetUserIdFromToken("bad-token"))
+                .ReturnsAsync((int?)null);
+
+            var controller = new OrderController(orderService.Object, authService.Object);
+
+            // Act
+            var result = await controller.CreateFromCart("bad-token");
+
+            // Assert
+            Assert.IsType<UnauthorizedResult>(result);
+        }
     }
 }
