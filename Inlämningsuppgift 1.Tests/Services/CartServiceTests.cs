09dd922bf9c803de3c8d98a93033042a1aff39b8
Jesper Stranne <179859375+Jeppzore@users.noreply.github.com>
2025-12-13 11:01:47 +0100

    Update CartServiceTests.cs
    Created 2 new tests to demonstrate FAKES to verify real cart behaviour & use a fake repository.
    
    Tests include adding an item and itemQuantity if said item already exists. And a test for clearing the cart for the specific userId

--- Inlämningsuppgift 1.Tests/Services/CartServiceTests.cs
+++ Inlämningsuppgift 1.Tests/Services/CartServiceTests.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Inlämningsuppgift_1.Repositories.Carts;
+using Inlämningsuppgift_1.Services;
+using Moq;
 
 namespace Inlämningsuppgift_1.Tests.Services
 {
-    internal class CartServiceTests
+    public class CartServiceTests
     {
+        [Fact]
+        public async Task AddItem_AddsQuantity_WhenProductAlreadyExists()
+        {
+            // Arrange (FAKE)
+            var cartRepo = new CartRepository();
+            var productService = new Mock<IProductService>().Object;
+
+            var service = new CartService(cartRepo, productService);
+
+            // Act
+
+            await service.AddItem(1, productId: 10, quantity: 2);
+            await service.AddItem(1, productId: 10, quantity: 3);
+
+            var cart = await service.Getcart(1);
+
+            // Assert
+            Assert.Single(cart.Items);
+            Assert.Equal(5, cart.Items[0].Quantity);
+        }
+
+        [Fact]
+        public async Task ClearCart_RemovesAllItems()
+        {
+            // Arrange
+            var cartRepo = new CartRepository();
+            var productService = new Mock<IProductService>().Object;
+            var service = new CartService(cartRepo, productService);
+
+            await service.AddItem(1, 10, 2);
+
+            // Act
+            await service.ClearCart(1);
+            var cart = await service.Getcart(1);
+
+            // Assert
+            Assert.Empty(cart.Items);
+        }
     }
 }
